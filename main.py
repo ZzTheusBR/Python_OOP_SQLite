eeb6d913f6e8437a08ae4edbc39ac0a0ae8a086f
Matheus Barbosa Pereira <111500704+ZzTheusBR@users.noreply.github.com>
2022-11-24 01:18:49 +0000

    Consulta a banco, mas... a parte de inserir no banco está com algum erro

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Importa nossa arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 matheus = Pessoa(1, "Matheus Barbosa")
@@ -7,3 +9,27 @@ print(matheus)
 
 #Quero mostrar só compile
 print(matheus.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO...")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa (0, "Denzel Washington")
+
+#Olha que simples...
+novo = pessoaDAO.save(novo)
+
+#Consulta o banco de novo...
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
